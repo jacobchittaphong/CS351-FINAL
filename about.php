9cf6f5d605d1f4bdff11e909215eca1b03faff80
Jacob Chittaphong <122504215+jacobchittaphong@users.noreply.github.com>
2024-11-29 20:08:28 -0600

    about.php wip

--- about.php
+++ about.php
@@ -17,3 +17,15 @@ session_start();
         <p>
             Welcome to <strong>The Club Compass</strong>, your go-to destination for discovering the best golf courses tailored to your preferences. Whether you're a seasoned golfer or just starting your journey, we aim to simplify your golfing experience by providing detailed course information, reviews, and personalized recommendations.
         </p>
+        <p>
+            Our mission is to connect golfers with courses that suit their skill levels, locations, and amenities. From local favorites to world-reowned courses, we make it easy to find and explore your next golfing adventure. 
+        </p>  
+    </section>
+
+    <section class="mission-section">
+        <h3>Our Mission</h3>
+        <p>
+            At <strong>The Club Compass</strong>, our mission is to inspire and assist golfers of all levels providing a comprehensive platform to discover, review, and connect with golf courses worldwide. We are dedicated to enhancing the golfing experience and fostering a community of enthusiast who share a passion for the game.
+        </p>
+    </section>
+    
