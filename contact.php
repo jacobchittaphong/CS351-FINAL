95c10a84a6646caa288c69c3c5313e67ca19a96a
Jacob Chittaphong <122504215+jacobchittaphong@users.noreply.github.com>
2024-12-02 23:09:27 -0500

    contact page wip

--- contact.php
+++ contact.php
@@ -9,3 +9,9 @@ session_start();
     <link rel="stylesheet" href="styles.css">
 </head>
 <?php include 'header.php'; ?>
+<section class="contact-section">
+        <h2>Contact Us</h2>
+        <p>
+            We value your feedback and would love to hear from you! If you have any questions, suggestions, or need assistance, please use the form below to reach out to us.
+        </p>
+</section>
