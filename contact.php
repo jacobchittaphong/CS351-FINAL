1f1fe18d371d41eb2b87eb83c259154eead3e5e3
Jacob Chittaphong <122504215+jacobchittaphong@users.noreply.github.com>
2024-12-02 23:08:23 -0500

    contact page wip

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us - The Club Compass</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
